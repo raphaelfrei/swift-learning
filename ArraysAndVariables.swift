2b7c6cfdb39dec8cef750bbc92572a04b8028199
Raphael Frei <raphaelrodriguesfrei@gmail.com>
2024-10-03 17:46:39 -0300

    Update ArraysAndVariables.swift

--- ArraysAndVariables.swift
+++ ArraysAndVariables.swift
@@ -1,59 +1,33 @@
 //
-//  ArraysAndVariables.swift
+//  ViewController.swift
 //  Dicee-iOS13
 //
 
 import UIKit
 
-class ArraysAndVariables: UIViewController {
+class ViewController: UIViewController {
 
 
     @IBOutlet weak var imgDice1: UIImageView!
     @IBOutlet weak var imgDice2: UIImageView!
-    
-    // Does not specify the type of the variable
-    var diceOneNumber = 0
-    var diceTwoNumber = 5
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-    }
+
 
     @IBAction func RollButtonPress(_ sender: UIButton) {
         
+        let diceImages = [UIImage(imageLiteralResourceName: "DiceOne"),
+                          UIImage(imageLiteralResourceName: "DiceTwo"),
+                          UIImage(imageLiteralResourceName: "DiceThree"),
+                          UIImage(imageLiteralResourceName: "DiceFour"),
+                          UIImage(imageLiteralResourceName: "DiceFive"),
+                          UIImage(imageLiteralResourceName: "DiceSix")]
+        
         // The first [] shows an array with all elements
         // The second [] shows which element you want to use
-        imgDice1.image =
-        [UIImage(imageLiteralResourceName: "DiceOne"),
-         UIImage(imageLiteralResourceName: "DiceTwo"),
-         UIImage(imageLiteralResourceName: "DiceThree"),
-         UIImage(imageLiteralResourceName: "DiceFour"),
-         UIImage(imageLiteralResourceName: "DiceFive"),
-         UIImage(imageLiteralResourceName: "DiceSix")][diceOneNumber]
+        imgDice1.image = diceImages[Int.random(in: 0...5)]
         
-        imgDice2.image =
-        [UIImage(imageLiteralResourceName: "DiceOne"),
-         UIImage(imageLiteralResourceName: "DiceTwo"),
-         UIImage(imageLiteralResourceName: "DiceThree"),
-         UIImage(imageLiteralResourceName: "DiceFour"),
-         UIImage(imageLiteralResourceName: "DiceFive"),
-         UIImage(imageLiteralResourceName: "DiceSix")][diceTwoNumber]
+        imgDice2.image = diceImages[Int.random(in: 0...5)]
         
-        // Itinerate thru every image on press of the button
-        if (diceOneNumber < 5) {
-            diceOneNumber += 1
-        } else {
-            diceOneNumber = 0
-        }
         
-        // Inverse itineration
-        if (diceTwoNumber > 0){
-            diceTwoNumber -= 1
-        } else {
-            diceTwoNumber = 5
-        }
         
     }
     
